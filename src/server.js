b2f0b9c284c6fb9f389746b6bc303952a99d2c54
himanshu thakur <thakurhimanshu952@gmail.com>
2023-05-27 23:40:49 +0530

    MY-BLOG-BACKEND

---
+++ src/server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import { db, connectToDb } from './db.js';
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+
+    const article = await db.collection('articles').findOne({ name });
+    console.log(name);
+    if (article) {
+        res.json(article);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.put('/api/articles/:name/upvote', async (req, res) => {
+    const { name } = req.params;
+   
+    await db.collection('articles').updateOne({ name }, {
+        $inc: { upvotes: 1 },
+    });
+    const article = await db.collection('articles').findOne({ name });
+
+    if (article) {
+        res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+    } else {
+        res.send('That article doesn\'t exist');
+    }
+});
+
+app.post('/api/articles/:name/comments', async (req, res) => {
+    const { name } = req.params;
+    const { postedBy, text } = req.body;
+
+    await db.collection('articles').updateOne({ name }, {
+        $push: { comments: { postedBy, text } },
+    });
+    const article = await db.collection('articles').findOne({ name });
+
+    if (article) {
+        res.send(article.comments);
+    } else {
+        res.send('That article doesn\'t exist!');
+    }
+});
+
+connectToDb(() => {
+    console.log('Successfully connected to database!');
+    app.listen(8000, () => {
+        console.log('Server is listening on port 8000');
+    });
+})
